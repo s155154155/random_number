0c7bd1f04a1956d84f094ecdc4bd287574028f2d
NomikLee <nomiklee@liyuanhaode-MacBook-Pro.local>
2021-01-02 17:22:07 +0800

    basic version

---
+++ rd_nb.py
@@ -0,0 +1,15 @@
+import random
+
+r = random.randint(1 , 100)
+
+while True:
+	num = input('1~100猜一個數字')
+	num = int(num)
+	
+	if num == r:
+		print('終於猜對了！')
+		break
+	elif num > r:
+		print('比答案大')
+	else:
+		print('比答案小')
